18f9e6bea1822a6665f447d6c2f66071a88a3967
symon254 <79975499+symon254@users.noreply.github.com>
2022-04-14 10:39:39 +0300

    feat(TableData.js): table data
    creating the table data for testing

---
+++ src/components/TablesComponent/TableData.js
@@ -0,0 +1,15 @@
+export const thData = ["name", "email", "date"];
+export const tbData = [
+    {
+        id: "1",
+        items: ["simon", "simon@gmail.com", "01/01/2022"],
+    },
+    {
+        id: "2",
+        items: ["effie", "effie@gmail.com", "01/01/2022"],
+    },
+    {
+        id: "3",
+        items: ["sydo", "sydo@gmail.com", "01/01/2022"],
+    },
+];
