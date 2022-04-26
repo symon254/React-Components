313c6a06331d9a6982b89edba4ebc1340072569b
symon254 <79975499+symon254@users.noreply.github.com>
2022-04-26 15:47:11 +0300

    feat(Data.js): data
    home page data

---
+++ src/Data/Data.js
@@ -0,0 +1,29 @@
+import React from "react";
+export const DataSet1 = [
+    {
+        label: (
+            <>
+                <h6
+                    style={{
+                        width: "500px",
+                        textAlign: "left",
+                        marginTop: "-30px",
+                    }}
+                >
+                    Sample Example Sample Example Sample Example Sample Example
+                    Sample Example Sample Example Sample Example Sample Example
+                    Sample Example Sample Example Sample Example Sample Example
+                    Sample Example Sample Example Sample Example Sample Example
+                    Sample Example Sample Example Sample Example Sample Example
+                    Sample Example Sample Example Sample Example Sample Example
+                    Sample{" "}
+                </h6>
+            </>
+        ),
+    },
+];
+export const DataSet2 = [
+    {
+        label: <></>,
+    },
+];
