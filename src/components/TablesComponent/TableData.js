ea9404479c84e566b5854431339987ea4e56e326
symon254 <79975499+symon254@users.noreply.github.com>
2022-04-26 15:45:04 +0300

    refactor(TableData.js): .
    .

--- src/components/TablesComponent/TableData.js
+++ src/components/TablesComponent/TableData.js
@@ -1,15 +1,51 @@
-export const thData = ["name", "email", "date"];
+import React, { useState } from "react";
+import Button from "../Buttons/Buttons";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import Modals from "../Modal/Modal";
+
+export const thData = ["name", "email", "date", "Actions"];
 export const tbData = [
     {
         id: "1",
-        items: ["simon", "simon@gmail.com", "01/01/2022"],
+        items: [
+            "simon",
+            "simon@gmail.com",
+            "01/01/2022",
+            <Button startIcon={<FaEdit />} styleClass="btn-success" />,
+            <Button
+                startIcon={<MdDelete />}
+                styleClass="btn-danger"
+                marginLeft="-90px"
+            />,
+        ],
     },
     {
         id: "2",
-        items: ["effie", "effie@gmail.com", "01/01/2022"],
+        items: [
+            "effie",
+            "effie@gmail.com",
+            "01/01/2022",
+            <Button startIcon={<FaEdit />} styleClass="btn-success" />,
+            <Button
+                startIcon={<MdDelete />}
+                styleClass="btn-danger"
+                marginLeft="-90px"
+            />,
+        ],
     },
     {
         id: "3",
-        items: ["sydo", "sydo@gmail.com", "01/01/2022"],
+        items: [
+            "sydo",
+            "sydo@gmail.com",
+            "01/01/2022",
+            <Button startIcon={<FaEdit />} styleClass="btn-success" />,
+            <Button
+                startIcon={<MdDelete />}
+                styleClass="btn-danger"
+                marginLeft="-90px"
+            />,
+        ],
     },
 ];
