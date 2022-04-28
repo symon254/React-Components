e212bcb1eab9ed4f4e30bc8cb8d1d7f3f3fc386d
symon254 <79975499+symon254@users.noreply.github.com>
2022-04-28 13:38:18 +0300

    data
    additional data set

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,5 +1,5 @@
 import React from "react";
-export const DataSet1 = [
+export const Images = [
     {
         label: (
             <>
@@ -10,34 +10,137 @@ export const DataSet1 = [
                         marginTop: "-30px",
                     }}
                 >
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample{" "}
+                    Images may be two or three-dimensional, such as a photograph
+                    or screen display, or three-dimensional, such as a statue or
+                    hologram. They may be captured by optical devices – such as
+                    cameras, mirrors, lenses, telescopes, microscopes, etc. and
+                    natural objects and phenomena, such as the human eye or
+                    water.
                 </h6>
             </>
         ),
     },
 ];
-export const DataSet2 = [
+export const Headings = [
     {
         label: (
             <>
-                <h6 style={{ textAlign: "center" }}>
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example Sample Example Sample Example Sample Example
-                    Sample Example{" "}
+                <h5 style={{ textAlign: "center" }}>
+                    A sample is just a part of a population. For example, let’s
+                    say your population was every American, and you wanted to
+                    find out how much the average person earns. Time and
+                    finances stop you from knocking on every door in America, so
+                    you choose to ask 1,000 random people. This one thousand
+                    people is your sample.{" "}
+                </h5>
+            </>
+        ),
+    },
+];
+export const Videos = [
+    {
+        label: (
+            <>
+                <h6
+                    style={{
+                        width: "500px",
+                        textAlign: "left",
+                        marginTop: "-30px",
+                    }}
+                >
+                    Video technology was first developed for mechanical
+                    television systems, which were quickly replaced by
+                    cathode-ray tube (CRT) television systems, but several new
+                    technologies for video display devices have since been
+                    invented. Video was originally exclusively a live
+                    technology.
+                </h6>
+            </>
+        ),
+    },
+];
+export const Audios = [
+    {
+        label: (
+            <>
+                <h6
+                    style={{
+                        width: "500px",
+                        textAlign: "left",
+                        marginTop: "-30px",
+                    }}
+                >
+                    An audio signal is a representation of sound, typically
+                    using either a changing level of electrical voltage for
+                    analog signals, or a series of binary numbers for digital
+                    signals. Audio signals have frequencies in the audio
+                    frequency range of roughly 20 to 20,000 Hz, which
+                    corresponds to the lower and upper limits of human hearing.
+                </h6>
+            </>
+        ),
+    },
+];
+export const Addverts = [
+    {
+        label: (
+            <>
+                <h6
+                    style={{
+                        width: "500px",
+                        textAlign: "left",
+                        marginTop: "-30px",
+                    }}
+                >
+                    Advertising is a marketing communication that employs an
+                    openly sponsored, non-personal message to promote or sell a
+                    product, service or idea.[1]: 465  Sponsors of advertising
+                    are typically businesses wishing to promote their products
+                    or services. Advertising is differentiated from public
+                    relations in that an advertiser pays for and has control
+                    over the message.
+                </h6>
+            </>
+        ),
+    },
+];
+export const Documents = [
+    {
+        label: (
+            <>
+                <h6
+                    style={{
+                        width: "500px",
+                        textAlign: "left",
+                        marginTop: "-30px",
+                    }}
+                >
+                    A document is a written, drawn, presented, or memorialized
+                    representation of thought, often the manifestation of
+                    non-fictional, as well as fictional, content. The word
+                    originates from the Latin Documentum, which denotes a
+                    "teaching" or "lesson": the verb doceō denotes "to teach".
+                </h6>
+            </>
+        ),
+    },
+];
+export const Texts = [
+    {
+        label: (
+            <>
+                <h6
+                    style={{
+                        width: "500px",
+                        textAlign: "left",
+                        marginTop: "-30px",
+                    }}
+                >
+                    In literary theory, a text is any object that can be "read",
+                    whether this object is a work of literature, a street sign,
+                    an arrangement of buildings on a city block, or styles of
+                    clothing. It is a coherent set of signs that transmits some
+                    kind of informative message.
                 </h6>
             </>
         ),
