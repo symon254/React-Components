78482a6ba39bf85087aada7a19fdd8de2bb5dce2
symon254 <79975499+symon254@users.noreply.github.com>
2022-04-28 12:43:44 +0300

    refactor(Index.js): ,
    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from "./App";
-import Home from "./Pages/Home";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Home />
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
