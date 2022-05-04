5a3dd1f847c3aa2905da2eb239f116a5c35a509e
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-04 11:52:10 +0300

    navigation bar
    navigation bar

--- src/Pages/pageComponents/Navigation.js
+++ src/Pages/pageComponents/Navigation.js
@@ -1,86 +1,171 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Modals from "../../components/Modal/Modal";
-import Button from "../../components/Buttons/Buttons";
 import Tables from "../Tables";
 import Home from "../Home";
+import Components from "../Components";
+import "./../Pages.css";
+import { SiWebcomponentsdotorg } from "react-icons/si";
+import {
+    FaHome,
+    FaUserAlt,
+    FaHandsHelping,
+    FaInfoCircle,
+} from "react-icons/fa";
+import { MdSettings } from "react-icons/md";
+import { BiTable } from "react-icons/bi";
+import { IoLogoTux, IoMdLogOut } from "react-icons/io";
 
 const Navigation = () => {
-    const [handleShowBlue, setHandleShowBlue] = useState(false);
-    const [handleShowRed, setHandleShowRed] = useState(false);
-
     return (
         <div>
             <Router>
+                <div style={{ marginBottom: "30px" }}></div>
                 <Navbar bg="primary" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="/Home">Home-Page</Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Link to={"/Home"} className="nav-link">
-                                Home
-                            </Link>
-                            <Link to={"/Tables"} className="nav-link">
-                                Tables
-                            </Link>
+                    <Container className="navigation-main ">
+                        <Navbar.Brand href="/Home">
+                            <IoLogoTux
+                                className="icon-home"
+                                style={{
+                                    marginRight: "10px",
+                                    fontSize: "30px",
+                                }}
+                            />
+                            Simon
+                        </Navbar.Brand>
+                        <Nav className="justify-content-end">
+                            <div className="navi ">
+                                <ul>
+                                    <li className="list ">
+                                        <a href="/Home">
+                                            <span className="icon">
+                                                <div className="div-icon-navi bg-primary">
+                                                    <FaHome
+                                                        style={{
+                                                            marginTop: "-40px",
+                                                        }}
+                                                    />
+                                                </div>
+                                            </span>
+
+                                            <span
+                                                style={{ marginLeft: "-5px" }}
+                                                className="text"
+                                            >
+                                                Home
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li className="list">
+                                        <a href="#">
+                                            <span className="icon">
+                                                <div className="div-icon-navi bg-primary">
+                                                    <FaUserAlt
+                                                        style={{
+                                                            marginTop: "-40px",
+                                                        }}
+                                                    />
+                                                </div>
+                                            </span>
+                                            <span
+                                                style={{ marginLeft: "-5px" }}
+                                                className="text"
+                                            >
+                                                profile
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li className="list">
+                                        <a href="/Components">
+                                            <span className="icon">
+                                                <div className="div-icon-navi bg-primary">
+                                                    <SiWebcomponentsdotorg
+                                                        style={{
+                                                            marginTop: "-40px",
+                                                        }}
+                                                    />
+                                                </div>
+                                            </span>
+                                            <span
+                                                style={{ marginLeft: "-5px" }}
+                                                className="text"
+                                            >
+                                                Component
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li className="list">
+                                        <a href="/Tables">
+                                            <span className="icon">
+                                                <div className="div-icon-navi bg-primary">
+                                                    <BiTable
+                                                        style={{
+                                                            marginTop: "-40px",
+                                                        }}
+                                                    />
+                                                </div>
+                                            </span>
+                                            <span
+                                                style={{ marginLeft: "-5px" }}
+                                                className="text"
+                                            >
+                                                Table
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li className="list">
+                                        <a href="#">
+                                            <span className="icon">
+                                                <div className="div-icon-navi bg-primary">
+                                                    <MdSettings
+                                                        style={{
+                                                            marginTop: "-40px",
+                                                        }}
+                                                    />
+                                                </div>
+                                            </span>
+                                            <span
+                                                style={{ marginLeft: "-5px" }}
+                                                className="text"
+                                            >
+                                                settings
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                             <NavDropdown
+                                className="dropdowns"
                                 title="Dropdown"
                                 id="basic-nav-dropdown"
                             >
                                 <NavDropdown.Item href="#action/3.1">
-                                    Action1
+                                    <FaUserAlt className="drop-icon" /> user
                                 </NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
-                                    Action2
+                                    <FaHandsHelping className="drop-icon" />{" "}
+                                    Help
                                 </NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">
-                                    Action3
+                                    <FaInfoCircle className="drop-icon" /> More
+                                    Info
                                 </NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">
-                                    Action4
+                                    <IoMdLogOut className="drop-icon" /> LogOut
                                 </NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
-
-                        <Button
-                            float="right"
-                            marginTop="-px"
-                            marginRight="20px"
-                            children="red"
-                            styleClass="btn-danger"
-                            width={"100px"}
-                            onClick={() => setHandleShowRed(true)}
-                        />
-                        <Button
-                            float="right"
-                            marginTop="-px"
-                            children="blue"
-                            styleClass="btn-success"
-                            width={"100px"}
-                            onClick={() => setHandleShowBlue(true)}
-                        />
                     </Container>
                 </Navbar>
                 <div className="container mt-3">
                     <Routes>
                         <Route path={"/Home"} element={<Home />} />
+                        <Route path={"/Components"} element={<Components />} />
                         <Route path={"/Tables"} element={<Tables />} />
                     </Routes>
                 </div>
             </Router>
-            <Modals
-                title="blue"
-                children="this is blue"
-                showModal={handleShowBlue}
-                onHideMode={() => setHandleShowBlue(false)}
-            />
-            <Modals
-                title="red"
-                children="this is red"
-                showModal={handleShowRed}
-                onHideMode={() => setHandleShowRed(false)}
-            />
         </div>
     );
 };
