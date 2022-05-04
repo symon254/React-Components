fe9b6efc33ece120efd7412e74e15c9dba5a9261
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-04 11:53:41 +0300

    table buttons
    table buttons for data manupilation

--- src/Data/ButtonsModal.js
+++ src/Data/ButtonsModal.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import Modals from "../components/Modal/Modal";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { tbData } from "../components/TablesComponent/TableData";
 
 export const Edit = () => {
     const [handleShowEdit, setHandleShowEdit] = useState(false);
-
     return (
         <div>
             <button
@@ -24,22 +24,29 @@ export const Edit = () => {
     );
 };
 export const Delete = () => {
-    const [handleDelete, setHandleDelete] = useState(false);
-
+    //const dataList = tbData;
+    const [handleDelete, setHandleDelete] = useState(tbData);
+    // const deletes = (id) => {
+    //     setHandleDelete(handleDelete.filter((item) => item.id !== id));
+    // };
+    const removes = () => {
+        const me = handleDelete.filter((item) => item.id !== tbData.id);
+        setHandleDelete(me);
+    };
     return (
         <div>
             <button
-                onClick={() => setHandleDelete(true)}
+                // onClick={(id) => removes(id)}
                 className="btn btn-danger"
             >
                 <MdDelete />
             </button>
-            <Modals
+            {/* <Modals
                 title="Delete"
                 children="the record cant be deleted at the moment"
                 showModal={handleDelete}
                 onHideMode={() => setHandleDelete(false)}
-            />
+            /> */}
         </div>
     );
 };
