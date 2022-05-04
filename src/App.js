be1007f07bc434968abd10095ab1e0fcdb190d51
symon254 <79975499+symon254@users.noreply.github.com>
2022-05-04 11:56:56 +0300

    footer
    adding footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from "./Pages/pageComponents/Navigation";
+//import Navigation from "./Pages/pageComponents/Navigation";
 import React from "react";
+import Footers from "./Pages/pageComponents/Footers";
+import Navigation from "./Pages/pageComponents/Navigation";
 function App() {
     return (
         <div className="App">
             <Navigation />
+            <Footers />
         </div>
     );
 }
